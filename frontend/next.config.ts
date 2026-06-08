7d449080d658eccd586bb48061c80c7af30c8b22
bijun <zhengbijun123@gmail.com>
2026-06-09 02:05:29 +0800

    fix home thumbnail

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hotinsert-api.zhengbijun123.workers.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // Google user avatars
+      },
+    ],
+  },
 };
 
 export default nextConfig;
